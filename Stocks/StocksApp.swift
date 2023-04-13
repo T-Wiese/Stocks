bcee315133542b42ccd7c9c27f04de973e0633c3
t-wiese <twiese@turntable.fm>
2023-04-13 11:50:10 -0700

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Torin Wiese (Work) on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
